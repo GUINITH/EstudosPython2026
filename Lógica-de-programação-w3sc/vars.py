e796510af06acecee64672bacfe10db080f15be4
Guilherme Felipe <fellipeguilherme37@gmail.com>
2026-03-11 22:21:33 -0300

    add estudos de variáveis

---
+++ Lógica-de-programação-w3sc/vars.py
@@ -0,0 +1,42 @@
+# Variáveis são a  forma de armazenar dados na memoria do computador. 
+# Podemos declarar variáveis, atribuir valores a elas e imprimir seus valores. 
+# Elas podem receber diversos  tipos de valores, como: strings, numeros, boleanos, listas, tuplas e dicionários. 
+
+# Exemplos de variáveis de declarar uma variável e atribuir um valor  a ela: 
+
+fruta = 'Pera'
+
+#  Exemplo de printar um valor de varável: 
+print(fruta)
+
+
+# Tipos de  dados que uma variável pode receber: 
+# strings: 
+nome = 'Guilherme' 
+print(nome)
+
+#números inteiros e reais:
+idade = 25
+print(idade)
+altura = 1.80
+print(altura)
+
+#booleanos: 
+isTrue = True
+print(isTrue)
+
+#listas: 
+alunos = ['Guilherme', 'Sergio', 'Thiago', 'Joaquim']
+print(alunos) 
+
+#tuplas: 
+pessoas = ('Guilherme', 'Sergio', 'Thiago', 'Joaquim')
+print(pessoas)
+
+#dicionários: 
+pessoa = {'nome': 'Guilherme', 'idade': 25, 'altura': 1.80}
+print(pessoa)
+
+
+
+
