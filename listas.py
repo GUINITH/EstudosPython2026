68d5147153e645330a1dbcd7a055c0741e27737f
Guilherme Felipe <fellipeguilherme37@gmail.com>
2026-03-13 23:42:29 -0300

    listas

---
+++ listas.py
@@ -0,0 +1,9 @@
+# Listas: Listas são coleções de valores, que podemos adicionar, remover, modificar,alterar, etc. 
+
+# Geralmente são criadas usando colchetes ou a função list().: 
+lista = ['Guilherme', 'Sergio', 'Thiago', 'Joaquim']
+print(lista)
+
+
+numeros  = list(range(10))
+print(numeros)
